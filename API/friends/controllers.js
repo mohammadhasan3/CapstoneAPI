36d513c98a4381ec0751b285127df6316824f825
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2021-01-11 19:54:59 +0300

    Added withdraw friend request and fetch relationships

--- API/friends/controllers.js
+++ API/friends/controllers.js
@@ -3,15 +3,67 @@ const { User, Friend } = require("../../db/models");
 exports.PENDING = 0;
 exports.ACCEPTED = 1;
 exports.DECLINED = 2;
-exports.BLOCKED = 4;
+exports.BLOCKED = 3;
+
+//FetchRelationship
+exports.fetchRelationship = async (userId, otherId, next) => {
+  try {
+    let rel = await Friend.findAll({
+      where: {
+        user1Id: userId,
+        user2Id: otherId,
+      },
+    });
+    console.log(rel);
+    if (rel.length === 0) {
+      rel = await Friend.findAll({
+        where: {
+          user1Id: otherId,
+          user2Id: userId,
+        },
+      });
+    }
+    console.log(rel);
+    return rel;
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.addFriend = async (req, res, next) => {
   try {
     req.body.user1Id = req.user.id;
     req.body.actionUser = req.user.id;
-    console.log(req.body);
     const newRelationship = await Friend.create(req.body);
-    console.log(newRelationship);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.withdrawRequest = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const otherId = req.body.user2Id;
+    const relationship = await this.fetchRelationship(userId, otherId, next);
+    console.log(relationship[0].actionUser);
+    if (relationship) {
+      if (relationship[0].actionUser === req.user.id) {
+        await Friend.update(
+          { status: null },
+          {
+            where: {
+              user1Id: req.user.id,
+              user2Id: req.body.user2Id,
+            },
+          }
+        );
+        res.status(204).end();
+      } else {
+        const err = new Error("Unauthorized");
+        err.status = 401;
+        next(err);
+      }
+    }
   } catch (error) {
     next(error);
   }
--- API/friends/routes.js
+++ API/friends/routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const { addFriend } = require("./controllers");
+const { addFriend, withdrawRequest } = require("./controllers");
 
 router.get("/", passport.authenticate("jwt", { session: false }), addFriend);
+router.put(
+  "/withdrawRequest",
+  passport.authenticate("jwt", { session: false }),
+  withdrawRequest
+);
 
 module.exports = router;
