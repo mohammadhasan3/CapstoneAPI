415de13c0727441358b249e92efad2120305bd90
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2021-01-11 20:01:43 +0300

    Fixed merge errors

--- API/friends/routes.js
+++ API/friends/routes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const { addFriend, withdrawRequest } = require("./controllers");
-
+const { sendRequest, withdrawRequest } = require("./controllers");
 
 router.put(
   "/withdrawRequest",
   passport.authenticate("jwt", { session: false }),
   withdrawRequest
-const { sendRequest } = require("./controllers");
+);
 
 router.get(
   "/sendRequest",
