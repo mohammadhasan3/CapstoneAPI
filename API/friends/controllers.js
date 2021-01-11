9723292a04a56fe8e03fd2a948dac15a427e3e09
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2021-01-11 20:03:08 +0300

    Fixed more merging errors....

--- API/friends/controllers.js
+++ API/friends/controllers.js
@@ -5,7 +5,6 @@ exports.ACCEPTED = 1;
 exports.DECLINED = 2;
 exports.BLOCKED = 3;
 
-
 //FetchRelationship
 exports.fetchRelationship = async (userId, otherId, next) => {
   try {
@@ -15,7 +14,7 @@ exports.fetchRelationship = async (userId, otherId, next) => {
         user2Id: otherId,
       },
     });
-    console.log(rel);
+
     if (rel.length === 0) {
       rel = await Friend.findAll({
         where: {
@@ -24,7 +23,6 @@ exports.fetchRelationship = async (userId, otherId, next) => {
         },
       });
     }
-    console.log(rel);
     return rel;
   } catch (error) {
     next(error);
@@ -43,6 +41,35 @@ exports.sendRequest = async (req, res, next) => {
       res.json(newRelationship);
     } else {
       console.log("You cannot friend yourself");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.withdrawRequest = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const otherId = req.body.user2Id;
+    const relationship = await this.fetchRelationship(userId, otherId, next);
+    console.log(relationship[0].actionUser);
+    if (relationship) {
+      if (relationship[0].actionUser === req.user.id) {
+        await Friend.update(
+          { status: null },
+          {
+            where: {
+              user1Id: req.user.id,
+              user2Id: req.body.user2Id,
+            },
+          }
+        );
+        res.status(204).end();
+      } else {
+        const err = new Error("Unauthorized");
+        err.status = 401;
+        next(err);
+      }
     }
   } catch (error) {
     next(error);
